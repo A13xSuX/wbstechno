604941e3b8e46bdcbe44ebc2e4222db04fb227b5
A13xSuX <al.2005.anisimov@gmail.com>
2025-09-07 11:58:32 +0300

    yow

--- cmd/orderservice/main.go
+++ cmd/orderservice/main.go
@@ -19,37 +19,54 @@ import (
 )
 
 func main() {
-
 	if err := godotenv.Load("../../.env"); err != nil {
 		log.Println("Не найден .env файл")
 	}
 
 	cfg := config.LoadConfig()
 
+	// Подключаемся к БД
 	db, err := database.ConnectDB(cfg.DB)
 	if err != nil {
 		log.Fatalf("Ошибка подключения к БД: %v", err)
 	}
+	defer func() {
+		log.Println("Закрываем соединение с БД")
+		if err := db.CloseWithTimeout(10 * time.Second); err != nil {
+			log.Printf("Ошибка закрытия БД: %v", err)
+		} else {
+			log.Println("Соединение с БД закрыто")
+		}
+	}()
 
+	// Создаем репозиторий
+	orderRepo := database.NewOrderRepository(db.DB)
+
+	// Создаем кэш
 	orderCache := cache.NewOrderCache(cfg.Cache.MaxSize, cfg.Cache.TTL)
-	sqlDB := db.DB
-	if err := cache.RestoreCacheFromDB(sqlDB, orderCache, cfg.Cache.RestoreLimit); err != nil {
+	defer orderCache.Stop()
+
+	// Восстанавливаем кэш из БД
+	if err := cache.RestoreCacheFromDB(db.DB, orderCache, cfg.Cache.RestoreLimit); err != nil {
 		log.Printf("Ошибка восстановления кэша: %v", err)
 	}
 
-	orderService := service.NewOrderService(sqlDB, orderCache)
+	// Создаем сервис
+	orderService := service.NewOrderService(orderRepo, orderCache)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	var wg sync.WaitGroup
 
+	// Запускаем HTTP сервер
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		handler.StartHTTPServer(ctx, orderService, cfg.HTTP.Port)
 	}()
 
+	// Запускаем Kafka consumer
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
@@ -69,16 +86,6 @@ func main() {
 
 	wg.Wait()
 	log.Println("HTTP сервер и Kafka consumer остановлены")
-
-	orderCache.Stop()
-	log.Println("Кэш остановлен")
-
-	log.Println("Закрываем соединение с БД")
-	if err := db.CloseWithTimeout(10 * time.Second); err != nil {
-		log.Printf("Ошибка закрытия БД: %v", err)
-	} else {
-		log.Println("Соединение с БД закрыто")
-	}
 
 	log.Println("Все компоненты остановлены")
 	time.Sleep(100 * time.Millisecond)
